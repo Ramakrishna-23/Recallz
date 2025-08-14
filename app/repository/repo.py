f1ad54755b2927af9e856a3ef27a30ac302731d6
ramakrishna f m <ramakrishnafm@ramakrishnas-MacBook-Air.local>
2025-08-14 10:16:06 +0530

    the db schema is completed

---
+++ app/repository/repo.py
@@ -0,0 +1,21 @@
+# this is repo layer the interaction layer between the db and the queries 
+
+# all the possbile actions for users
+# 1. get_user_id => user.py => get
+# 2. post_username_email => user.py => post 
+# 3. put_user_name => user.py => put
+# 4. put_email_id => user.py => put
+# 5. put_password => user.py => put
+# 6. delete_user => user.py => del
+
+
+### all the possible actions for bookmarks.py 
+# 1. get_all_bookmark => get
+# 2. get_bookmarsk_by_id => get
+# 3. get_bookmarks_by_type => get
+# 4. get_bookmarks_by_tags => get
+#.  Post
+# 5. post_bookmarks_by_id => creates the bookmark by id and store in db
+#   Put
+# 6. put_bookmarks_by_id_for_anytype => creates the bookmarks contents 
+# ### 
